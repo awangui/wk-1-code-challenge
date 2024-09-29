61aaf930e2fc485ed4fc6de13c12224e021d855d
Anita Wangui <anitawangui22@gmail.com>
2024-09-29 21:32:49 +0300

    add disability check

--- solutions/net-salary.js
+++ solutions/net-salary.js
@@ -1,14 +1,10 @@
-// Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary.
 const prompt = require("prompt-sync")();
 
 // Getting inputs and parsing them to integers
 let basicSalary = parseInt(prompt("Enter your basic salary: "));
 let benefits = parseInt(prompt("Enter your benefits: "));
 let insurance = parseInt(prompt("Enter your insurance: "));
-
-
-// Calculate gross salary (add all components)
-let grossSalary = basicSalary - benefits;
+let mortgage = parseInt(prompt("Enter your mortgage: "));
 
 // Function to calculate taxation
 function taxation(grossSalary, insurance) {
@@ -17,23 +13,14 @@ function taxation(grossSalary, insurance) {
   // Tax brackets and PAYE calculation
   if (grossSalary <= 24000) {
     paye = grossSalary * 0.1;
-  } 
-  else if (grossSalary > 24000 && grossSalary <= 32333) {
+  } else if (grossSalary > 24000 && grossSalary <= 32333) {
     paye = 24000 * 0.1 + (grossSalary - 24000) * 0.25;
-  } 
-  else if (grossSalary > 32333 && grossSalary <= 500000) {
+  } else if (grossSalary > 32333 && grossSalary <= 500000) {
     paye = 24000 * 0.1 + 8333 * 0.25 + (grossSalary - 32333) * 0.3;
-  } 
-  else if (grossSalary > 500000 && grossSalary <= 800000) {
-    paye =24000 * 0.1 + 8333 * 0.25 + 467667 * 0.3 + (grossSalary - 500000) * 0.325;
-  } 
-  else if (grossSalary > 800000) {
-    paye =
-      24000 * 0.1 +
-      8333 * 0.25 +
-      467667 * 0.3 +
-      300000 * 0.325 +
-      (grossSalary - 800000) * 0.35;
+  } else if (grossSalary > 500000 && grossSalary <= 800000) {
+    paye = 24000 * 0.1 + 8333 * 0.25 + 467667 * 0.3 + (grossSalary - 500000) * 0.325;
+  } else if (grossSalary > 800000) {
+    paye = 24000 * 0.1 + 8333 * 0.25 + 467667 * 0.3 + 300000 * 0.325 + (grossSalary - 800000) * 0.35;
   }
 
   // Reliefs
@@ -55,9 +42,20 @@ function taxation(grossSalary, insurance) {
 
   return paye;
 }
+let deductions = benefits+mortgage;
+// Calculate gross salary
+let grossSalary = basicSalary -deductions;
 
-// Calculate PAYE
-let paye = taxation(grossSalary, insurance);
+// Check disability status
+let disability = prompt('Do you have a disability exemption certificate? (yes/no) ');
+
+let paye = 0;
+if (disability.toLowerCase() === 'yes') {
+  paye = 0;
+} else {
+  paye = taxation(grossSalary, insurance);
+}
+
 
 // Calculate net salary
 let netSalary = grossSalary - paye;
