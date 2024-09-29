84e0b38e541ae1c0bf2601da58529d400527f3d1
Anita Wangui <anitawangui22@gmail.com>
2024-09-29 21:39:07 +0300

    modify solutions

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# wk-1-code-challenge
+# (Project Name)
+
+#### Date, 28/09/2024
+
+#### By *Anita Wangui*
+
+## Description
 Solution for phase 1 week 1 JavaScript code challenge
+
+
+## Installation
+You use git clone to be able to download the documents in the GitHub
+
+## Installation Requirements
+Git
+
+### Installation instruction
+```
+Git clone https://github.com/awangui/wk-1-code-challenge.git
+
+```
+
+# Live Link
+[Git](gh-pages link)
+
+## Technologies used
+HTML
+CSS
+Github
+Javascript
+
+## Support and contact details
+github.com/awangui
+
+### License
+The content of this site is licensed under the MIT license
+Copyright (c) 2018.
+
---
+++ solutions/speed-detector.js
@@ -0,0 +1,21 @@
+//Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points. For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+const prompt = require('prompt-sync')();
+let speed = prompt('Enter your speed: ');
+//function to detect speed
+function speedDetector(speed){
+    let points=0;
+    //checking if speed is valid
+    if(speed<70){
+        console.log('Ok');
+    }
+    else{
+        points=(speed-70)/5;
+        if(points>12){
+            console.log('License suspended');
+        }
+        else{
+            console.log('Points: '+points);
+        }
+    }
+}
+speedDetector(speed);
